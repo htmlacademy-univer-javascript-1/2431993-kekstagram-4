a00b28f0b8d1a51c82f9cb20980cb3b9a4f8ef47
alexandr <stepkin.04@inbox.ru>
2023-11-02 09:55:02 +0500

    module4

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+
 const MIN_INDEX_AVATAR = 1;
 const MAX_INDEX_AVATAR = 6;
+const COUNT_POSTS = 25;
 const MIN_COUNT_LIKES = 15;
 const MAX_COUNT_LIKES = 200;
 const MIN_COUNT_COMMENTS = 0;
@@ -11,7 +13,10 @@ const MESSAGES = [
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?'];
+
 const NAMES = ['Иван','Александр','Елизавета','Дарья', 'Сергей','Георгий'];
+
+const  DESCRIPTION = ['Прекрасная', 'Удивительная', 'Замечательная','Волшебная'];
 
 const getRandomNumber = (min,max) =>{
   const lower = Math.ceil(Math.min(min,max));
@@ -19,3 +24,29 @@ const getRandomNumber = (min,max) =>{
   const result = Math.random() * (upper-lower+1 ) + lower;
   return Math.floor(result);
 };
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const getComment = (id) => {
+  const comment = {
+    id,
+    avatar: `img/avatar-${getRandomNumber(MIN_INDEX_AVATAR, MAX_INDEX_AVATAR)}.svg`,
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
+  };
+  return comment;
+};
+
+const getPost = (id) => {
+  id++;
+  const photo = {
+    id,
+    url: `photos/${id}.jpg` ,
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomNumber(MIN_COUNT_LIKES, MAX_COUNT_LIKES),
+    comments: Array.from({length: getRandomNumber(MIN_COUNT_COMMENTS, MAX_COUNT_COMMENTS)},getComment)
+  };
+  return photo;
+};
+const generatePosts = () => Array.from({length:COUNT_POSTS}, getPost);
+generatePosts();
