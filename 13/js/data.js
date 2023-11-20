cb3e3f29dc47f900dfec3011e288e6a678905fce
Keks <keks@htmlacademy.ru>
2023-11-20 20:55:24 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/data.js
@@ -0,0 +1,22 @@
+const MIN_ID_PHOTO = 1;
+const MAX_ID_PHOTO = 25;
+const MIN_INDEX_AVATAR = 1;
+const MAX_INDEX_AVATAR = 6;
+const COUNT_POSTS = 25;
+const MIN_COUNT_LIKES = 15;
+const MAX_COUNT_LIKES = 200;
+const MIN_COUNT_COMMENTS = 0;
+const MAX_COUNT_COMMENTS = 30;
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?'];
+
+const NAMES = ['Иван','Александр','Елизавета','Дарья', 'Сергей','Георгий'];
+
+const  DESCRIPTION = ['Прекрасная', 'Удивительная', 'Замечательная','Волшебная'];
+
+export{MIN_INDEX_AVATAR, MAX_INDEX_AVATAR, COUNT_POSTS, MIN_COUNT_COMMENTS, MAX_COUNT_COMMENTS, MIN_COUNT_LIKES, MAX_COUNT_LIKES, NAMES, DESCRIPTION, MESSAGES, MIN_ID_PHOTO, MAX_ID_PHOTO};
---
+++ 13/js/renderPictures.js
@@ -0,0 +1,20 @@
+import {getPhotos} from '.';
+
+const picStorage = document.querySelector('.pictures');
+const picTemplate = document.querySelector('#picture').content;
+
+const photos = getPhotos();
+
+const picsListFragment = document.createDocumentFragment();
+
+photos.forEach(({url, description, likes, comments}) => {
+  const picElement = picTemplate.cloneNode(true);
+  picElement.querySelector('.picture__img').src = url;
+  picElement.querySelector('.picture__img').alt = description;
+  picElement.querySelector('.picture__comments').textContent = comments;
+  picElement.querySelector('.picture__likes').textContent = likes;
+  picsListFragment.appendChild(picElement);
+
+});
+
+picStorage.appendChild(picsListFragment);
