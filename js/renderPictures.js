0a29421b5df75d79bfc3c3d2942d49dc680dd4d3
alexandr <stepkin.04@inbox.ru>
2023-11-23 12:24:14 +0500

    add length for comments

--- js/renderPictures.js
+++ js/renderPictures.js
@@ -11,7 +11,7 @@ photos.forEach(({url, description, likes, comments}) => {
   const picElement = picTemplate.cloneNode(true);
   picElement.querySelector('.picture__img').src = url;
   picElement.querySelector('.picture__img').alt = description;
-  picElement.querySelector('.picture__comments').textContent = comments;
+  picElement.querySelector('.picture__comments').textContent = comments.length;
   picElement.querySelector('.picture__likes').textContent = likes;
   picsListFragment.appendChild(picElement);
 
