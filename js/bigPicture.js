c8199a43df1e02b15872978d9b4a891884226a19
AlexanderStpkn <stepkin.04@inbox.ru>
2023-12-03 16:31:44 +0500

    Добавление комментариев

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -1,11 +1,11 @@
 const bigPictureElement = document.querySelector('.big-picture');
-const commentCountElement = bigPictureElement.querySelector('.social__comment-count');
 const commentListElement = bigPictureElement.querySelector('.social__comments');
 const commentsLoaderElement = bigPictureElement.querySelector('.comments-loader');
+const bigPictureCommentsCount = bigPictureElement.querySelector('.comments-count');
+const bigPictureCommentsVisibleCount = bigPictureElement.querySelector('.comments-visible-count');
 const bodyElement = document.querySelector('body');
 const cancelButtonElement = bigPictureElement.querySelector('.big-picture__cancel');
 const commentElement = document.querySelector('.social__comment');
-
 const createComment = ({avatar, name, message}) =>{
   const comment = commentElement.cloneNode(true);
 
@@ -27,7 +27,20 @@ const renderComments = (comments) =>{
   commentListElement.append(fragment);
 };
 
+const loadComments = (maxVisibleComment, data) =>{
+  if(maxVisibleComment + maxVisibleComment > data.comments.length){
+    bigPictureCommentsVisibleCount.textContent = data.comments.length;
+    maxVisibleComment = data.comments.length;
+    commentsLoaderElement.classList.add('hidden');
+  }else{
+    bigPictureCommentsVisibleCount.textContent = maxVisibleComment += maxVisibleComment;
+  }
+  renderComments([...data.comments].splice(0,maxVisibleComment));
+};
+
 const hideBigPicture = () => {
+  commentsLoaderElement.removeEventListener('click', loadComments);
+  commentsLoaderElement.classList.remove('hidden');
   bigPictureElement.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeyDown);
@@ -44,23 +57,28 @@ const onCancelButtonClick = () => {
   hideBigPicture();
 };
 
-const renderPictureDetails = ({url, likes, description}) =>{
-  bigPictureElement.querySelector('.big-picture__img img').src = url;
-  bigPictureElement.querySelector('.big-picture__img img').alt = description;
-  bigPictureElement.querySelector('.likes-count').textContent = likes;
-  bigPictureElement.querySelector('.social__caption').textContent = description;
+const renderPictureDetails = (data) =>{
+  let MAX_VISIBLE_COUNT= 5;
+  bigPictureElement.querySelector('.big-picture__img img').src = data.url;
+  bigPictureElement.querySelector('.big-picture__img img').alt = data.description;
+  bigPictureElement.querySelector('.likes-count').textContent = data.likes;
+  bigPictureElement.querySelector('.social__caption').textContent = data.description;
+  bigPictureCommentsCount.textContent = data.comments.length;
+  bigPictureCommentsVisibleCount.textContent = data.comments.length < MAX_VISIBLE_COUNT ? data.comments.length : MAX_VISIBLE_COUNT;
+  if(MAX_VISIBLE_COUNT >= data.comments.length){
+    commentsLoaderElement.classList.add('hidden');
+  }
+  commentsLoaderElement.addEventListener('click', () =>{
+    loadComments(MAX_VISIBLE_COUNT, data)
+  })
+  renderComments([...data.comments].splice(0,MAX_VISIBLE_COUNT));
 };
 
 const showBigPicture = (data) => {
   bigPictureElement.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
-  commentsLoaderElement.classList.add('hidden');
-  commentCountElement.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeyDown);
-
   renderPictureDetails(data);
-  renderComments(data.comments);
-
 };
 
 cancelButtonElement.addEventListener('click', onCancelButtonClick);
